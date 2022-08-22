7c8d222f96b24a0dc63b590e7fecc2c7aac83017
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-08-22 22:28:47 +0530

    unwanted code removed from mainui

--- src/component/main-ui/MainUI.js
+++ src/component/main-ui/MainUI.js
@@ -3,21 +3,6 @@ import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export function MainUI() {
 
-    const navigate = useNavigate();
-
-    const location = useLocation();
-
-    useEffect(() => {
-
-        console.log(location.pathname);
-
-        if (location.pathname == '/') {
-
-            navigate('/geochat/signUp');
-        }
-
-    }, [])
-
     return (
         <div className="main-ui">
 
